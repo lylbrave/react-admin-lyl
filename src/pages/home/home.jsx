170c3bf357af0f7666be5f6012801b0381d82da3
李玉良 <38196886+lylbrave@users.noreply.github.com>
2021-06-18 14:14:42 +0800

    Update home.jsx

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
+import { Layout } from 'antd';
+import './home.css'
+import MySider from './components/sider'
+const { Header, Footer, Sider, Content } = Layout;
+
 class Home extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
-    render() { 
-        return ( 
-            <div>home</div>
-         );
+    render() {
+        return (
+            <Layout className="layout">
+                <Sider><MySider></MySider></Sider>
+                <Layout>
+                    <Header className="header">管理系统</Header>
+                    <Content>Content</Content>
+                    <Footer>Footer</Footer>
+                </Layout>
+            </Layout>
+        );
     }
 }
- 
+
 export default Home;
