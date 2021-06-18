8003e3b1868cf35cc7dd036e9965adc1e381e02c
李玉良 <38196886+lylbrave@users.noreply.github.com>
2021-06-18 14:13:16 +0800

    Update login.jsx

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -12,7 +12,7 @@ class Login extends Component {
     return (
       <div className="login">
         <LoginHeader className="header"></LoginHeader>
-        <LoginForm className="form"></LoginForm>
+        <LoginForm {...this.props} className="formWraper"></LoginForm>
       </div>
     );
   }
